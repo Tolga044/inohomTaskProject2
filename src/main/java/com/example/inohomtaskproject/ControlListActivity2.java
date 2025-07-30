6aacc96f2c8ba6e9fb34c66a87604bbca59d9102
Tolga044 <tolgapektas44@icloud.com>
2025-07-31 02:53:18 +0300

    İlk commit — proje dosyaları yüklendi Login ekranı eklendi Hata düzeltildi UI iyileştirildi

---
+++ src/main/java/com/example/inohomtaskproject/ControlListActivity2.java
@@ -0,0 +1,13 @@
+package com.example.inohomtaskproject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ControlListActivity2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_control_list2);
+}
+}
